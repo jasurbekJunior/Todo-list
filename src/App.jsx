677a1d938fdeb0b836f3326875ee111b64618850
jasurbek <jasurbeknabijoc@gmail.com>
2025-04-03 19:34:11 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [array,setArray] = useState([]);
-  const [value,setValue] = useState('');
+  const [array,setArray] = useState([])
+  const [value,setValue] = useState("")
 
-  
- const handleClick = (e) =>{
-  e.preventDefault();
-  array.push(value);
-  setValue("");
- }
+  const handleClick = (e) =>{
+e.preventDefault();
+    array.push(value);
+    setValue("");
+  }
   return (
     <div>
-      <form  onSubmit={handleClick}>
-
+      <form onSubmit={handleClick} >
         <input value={value} onChange={e => setValue(e.target.value)} type="text" />
         <button>add</button>
-
-         {array.map((text,id)=>(
-          <p key={id}>{text}</p>
-         ))}
+        {
+          array.map((text,id) => (
+            <p key={id}>{text}</p>
+          ))
+        }
       </form>
     </div>
   )
